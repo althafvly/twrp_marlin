3f876fb7377dd0b929d59bb0c3f4cd94d3258b4d
althafvly <althafvly@gmail.com>
2021-04-05 22:00:03 +0530

    TWRP-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,15 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_marlin.mk \
 	$(LOCAL_DIR)/aosp_sailfish.mk
 
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/omni_marlin.mk \
+	$(LOCAL_DIR)/omni_sailfish.mk
+
 COMMON_LUNCH_CHOICES := \
 	aosp_marlin-userdebug \
 	aosp_sailfish-userdebug
+
+COMMON_LUNCH_CHOICES := \
+	omni_marlin-userdebug \
+	omni_sailfish-userdebug
+
